1474e175939868dac6533a21e6cf1b570ee4aff9
Tayfun <ripeaglee@hotmail.com>
2023-09-15 10:46:05 +0200

    lunch opens auto when the page loads in menu

--- js/menu.js
+++ js/menu.js
@@ -12,26 +12,12 @@ function food(evt, foodmeni) {
   evt.currentTarget.className += " active";
 }
 
-// Function to open the default tab when the page loads
+// Function to open the "Lunch" tab by default when the page loads
 function openDefaultTab() {
-  // Hide all tab content except for the "thai" tab
-  var tabcontent = document.getElementsByClassName("tabcontent");
-  for (var i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Remove the "active" class from all tabs
-  var tablinks = document.getElementsByClassName("tablinks");
-  for (var i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Display the "thai" tab and set it as active
-  var thaiTab = document.getElementById("thai");
-  var thaiButton = document.querySelector(".tablinks[data-food='thai']");
-  if (thaiTab && thaiButton) {
-    thaiTab.style.display = "flex";
-    thaiButton.className += " active";
+  // Programmatically trigger a click event on the "Lunch" button
+  var lunchButton = document.getElementById("lunchButton");
+  if (lunchButton) {
+    lunchButton.click();
   }
 }
 
